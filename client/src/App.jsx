69cfc1be5c57bcbc36cf5af3f0b0d3d7babdd96e
mushroom <mushrxxxxxm@gmail.com>
2024-02-09 11:38:10 +0200

    create pages and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from './pages/home'
+import About from './pages/about'
+import SignIn from './pages/signin'
+import Dashboard from './pages/dashboard'
+import Projects from './pages/projects'
+import Signup from './pages/signup'
+
 export default function App() {
   return (
-    <div>
-      hello world
-    </div>
+    <BrowserRouter>
+    <Routes>
+       <Route path="/" element={<Home/>} />
+       <Route path="/about" element={<About/>} />
+       <Route path="/sign-in" element={<SignIn/>} />
+       <Route path="/sign-up" element={<Signup/>} />
+       <Route path="/dashboard" element={<Dashboard/>} /> 
+       <Route path="/projects" element={<Projects/>} />
+    </Routes>
+    
+    </BrowserRouter>
+    
   )
 }
